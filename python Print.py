db58dafad5a4b4ab103b15dddc22509d43c7932e
SahilKr4All <101191455+SahilKr4All@users.noreply.github.com>
2022-04-16 13:05:46 +0530

    Add files via upload

---
+++ python Print.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 20
+#comments
+#print("The sum of ",number1,"and",number2,"is",number1+number2)
+#print("The sum of {} and {} is {}".format(number1,number2,number1+number2))
+print(f"The sum of {number1} and {number2} is {number1+number2}")
