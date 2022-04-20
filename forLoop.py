5a225970e840ca3b7201060419fba5853de0f4d8
SahilKr4All <101191455+SahilKr4All@users.noreply.github.com>
2022-04-20 15:59:31 +0530

    Add files via upload

---
+++ forLoop.py
@@ -0,0 +1,53 @@
+'''
+Loop - repetition - for , while
+here in for-loop ,if start<end ending range is n-1
+'''
+'''
+for i in range(1,11,2):
+    print(i)
+
+'''
+# if end < start , end = n+1
+'''
+for i in range(10,0,-1):
+    print(i)
+
+'''
+'''
+for i in reversed(range(1,11)):
+    print(i)
+
+'''
+'''
+for i in range(2,21):
+    print(i)
+'''
+
+'''
+for i in reversed(range(2,21)):
+    print(i)
+'''
+'''
+for i in range(100,1000):
+    print(i)
+'''
+'''
+for i in range(1,11):
+    print(f"2*{i}= {2*i}")
+'''
+
+# result=0
+# for i in range(1,11):
+#     result= result+i
+# print(result)
+
+# sum= 0
+# for i in range(10,21):
+#     sum= sum+i
+# print(sum)
+
+# sum= 0
+# for i in range(1,11):
+#     sum= sum-i
+# print(sum)
+
