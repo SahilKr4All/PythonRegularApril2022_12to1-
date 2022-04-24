171d6d48efefda1621472a1b5de1be9a5272293a
SahilKr4All <101191455+SahilKr4All@users.noreply.github.com>
2022-04-24 14:34:09 +0530

    Add files via upload

--- forLoop.py
+++ forLoop.py
@@ -36,9 +36,10 @@ for i in range(1,11):
     print(f"2*{i}= {2*i}")
 '''
 
-# result=0
-# for i in range(1,11):
-#     result= result+i
+'''
+ result=0
+ for i in range(1,11):
+     result= result+i
 # print(result)
 
 # sum= 0
@@ -50,4 +51,70 @@ for i in range(1,11):
 # for i in range(1,11):
 #     sum= sum-i
 # print(sum)
+'''
+'''
+*
+**
+***
+****
+*****
+'''
+'''
+for i in range(1,6):
+    print("*"*i)
+'''
 
+'''
+    *
+   **
+  ***
+ ****
+*****
+'''
+
+# for i in range(1,6):
+#     print(" "*(5-i)+"*"*i)
+
+# break ,continue ,pass
+# for i in range(1,10000000000001):
+#     print(i)
+#     if i==10:
+#         break
+
+
+# for i in range(1,11):
+#     if (i%2==0):
+#         continue
+
+#     print(i)
+'''
+for i in range(1,2323):
+    pass
+'''
+# for i in range( 50,100):
+#     print(i)
+#     if i==99:
+#         break
+
+#nested Loop=>
+# 1
+# 22
+# 333
+# 4444
+# 55555
+'''
+for i in range(1,6):
+    for j in range(i):
+        print(i,end="")
+    print(end="\n")
+'''
+
+# 1
+# 12
+# 123
+# 1234
+# 12345
+for i in range(1,6):
+    for j in range(i):
+        print(i,end="")
+    print(end="\n")
