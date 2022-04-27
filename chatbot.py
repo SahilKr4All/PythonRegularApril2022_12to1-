a355c63fb0eaaaaf9b9f5faa6d86e6e0550f7284
SahilKr4All <101191455+SahilKr4All@users.noreply.github.com>
2022-04-27 12:40:57 +0530

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -4,20 +4,23 @@ import os,random,glob
 helloIntent = "hi","hie","hey","hello","hey bro","wassup"
 timeIntent = "time","time please","whats the time","show me time"
 dateIntent = "date","time date","whats the date","show me date"
-msg = input("Enter Message")
-if msg in  helloIntent:
-    print("Hi")
-elif msg in timeIntent:
-    dt = datetime.now()
-    print(dt.strftime("%I:%M:%S,%p"))
-elif msg in dateIntent:
-    dt = datetime.now()
-    print(dt.strftime("%d/%m/%Y,%a"))
-elif msg=="music":
-    x = glob.glob("*.mp3")
-    os.startfile(random.choice(x))
-    
-elif msg =="bye":
-    print("Good Bye..Tc..")
-else:
-    print("Sorry i dont understand")
+chat=True
+while chat == True:
+    msg = input("Enter Message")
+    if msg in  helloIntent:
+        print("Hi")
+    elif msg in timeIntent:
+        dt = datetime.now()
+        print(dt.strftime("%I:%M:%S,%p"))
+    elif msg in dateIntent:
+        dt = datetime.now()
+        print(dt.strftime("%d/%m/%Y,%a"))
+    elif msg=="music":
+        x = glob.glob("*.mp3")
+        os.startfile(random.choice(x))
+        
+    elif msg =="bye":
+        print("Good Bye..Tc..")
+        chat=False
+    else:
+        print("Sorry i dont understand")
---
+++ whileLoop.py
@@ -0,0 +1,22 @@
+'''
+for i in range(1,11):
+    print(i)
+
+x=1
+while (x<=10):
+    print(x)
+    x=x+2
+'''
+
+'''
+x=2
+while(x<=10):
+    print(x)
+    x=x+2
+'''
+result=0
+x=1
+while(x<=10):
+     result= result+x
+     x+=1
+print(result)
