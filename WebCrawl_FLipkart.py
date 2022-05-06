72a6c2161db4dbce12e4a35c0639ccb52b099fc6
Sahil Kumar <101191455+SahilKr4All@users.noreply.github.com>
2022-05-06 15:58:06 +0530

    Add files via upload

--- WebCrawl_FLipkart.py
+++ WebCrawl_FLipkart.py
@@ -1,3 +1,4 @@
+from re import A
 import urllib.request as url
 import bs4
 homePath = "https://www.flipkart.com"
@@ -14,3 +15,20 @@ p_name = data.find("span",class_="B_NuCI")
 print(f"Product Name => {p_name.text}")
 p_pricing = data.find("div",class_="_30jeq3 _16Jk6d")
 print(f"Product Price => {p_pricing.text}")
+div = data.find("div",class_="_2418kt")
+highlight_list = div.findAll("li",class_="_21Ahn-")
+
+print("HighLights = >")
+for item in highlight_list:
+    print("         ",item.text)
+
+div= data.find("div",class_="col JOpGWq")
+a = div.findAll("a")[-1]["href"]; 
+path = homePath+a
+
+res = url.urlopen(path)
+data = bs4.BeautifulSoup(res,'lxml')
+p = data.findAll("p",class_="_2-N8zT")
+print("Reviews = >")
+for item in p:
+    print("         ",item.text)
